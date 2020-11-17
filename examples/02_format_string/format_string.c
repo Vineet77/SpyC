394543f747f9c5933fbd7135f6dbff5e22612775
Biegaj, Konrad <Konrad.Biegaj@transunion.com>
2020-11-16 20:49:09 -0600

    Add format string example

---
+++ examples/02_format_string/format_string.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <emscripten.h>
+int main(int argc, const char *argv[]) {
+char bof[] = "AAAA";
+printf("%x.%x.%x.%x.%x.\n");
+return 0;
+}
+
+/*https://i.blackhat.com/us-18/Thu-August-9/us-18-Lukasiewicz-WebAssembly-A-New-World-of-Native_Exploits-On-The-Web-wp.pdf */
+
