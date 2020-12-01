f74b4d8d38027b429147cd4d144ea0d5db241994
Vineet77 <vineetrpatel@gmail.com>
2020-12-01 01:45:36 -0600

    got basic malloc overflow detection:

--- examples/03_string/analysis.js
+++ examples/03_string/analysis.js
@@ -1,8 +1,53 @@
+let mallocLocation;
+let mallocSize;
+let mallocs = [];
+let prevMallocStores = [];
 
-(function() {
+function getNextAddr(op, addr) {
+    switch (op) {
+        case "i32.store":
+            return addr + 4;
+        case "i32.store8":
+            return addr + 1;
+    }
+    return addr + 4; //default
+}
 
-    let mallocLocation;
-    let mallocSize
+
+function analyizeMallocHistoy() {
+    const begin = prevMallocStores[0].addr;
+    const lastMallocStore = prevMallocStores[prevMallocStores.length - 1]
+    const end = getNextAddr(lastMallocStore.op, lastMallocStore.addr);
+    const mallocRegion = mallocs.find(m => begin >= m.addr && begin <= m.addr);
+    console.log("Potential buffer overflow. Continuous allocation of size", end - begin, "at", "0x" + begin.toString(16), "overflowing malloc region at", "0x" + mallocRegion.addr.toString(16), "of size", mallocRegion.size);
+}
+
+
+(function () {
+
+
+
+
+
+
+
+    function updateMallocHistory(op, addr, inMallocRegion) {
+        if (prevMallocStores.length === 0) {
+            if (inMallocRegion) prevMallocStores.push({ op, addr })
+
+            return;
+        } else {
+            const lastStore = prevMallocStores[prevMallocStores.length - 1];
+            const endAddr = getNextAddr(lastStore.op, lastStore.addr);
+            //check is addr is the next addr to be loaded
+            if (endAddr === addr) {
+                prevMallocStores.push({ op, addr });
+            } else { //reset the malloc history
+                analyizeMallocHistoy();
+                prevMallocStores = inMallocRegion ? [{ op, addr }] : []
+            }
+        }
+    }
 
     function fctName(fctId) {
         const fct = Wasabi.module.info.functions[fctId];
@@ -10,6 +55,12 @@
         if (fct.import !== null) return fct.import;
         return fctId;
     }
+
+    function inSequenceStore(op, addr) {
+        if (prevMallocStore === null) return false;
+        if (op !== prevMallocStore.op) return false;
+    }
+
 
     Wasabi.analysis = {
         start(location) {
@@ -20,31 +71,39 @@
             if (fctName(targetFunc) === "malloc") {
                 mallocLocation = location;
                 mallocSize = args[0];
-                console.log('Call malloc');
             }
         },
 
         call_post(location, values) {
             if (mallocLocation !== undefined && mallocLocation.func == location.func && mallocLocation.instr == location.instr) {
-                console.log(location, "malloc of size", mallocSize, "got address", values[0]);
+                console.log("malloc call of size", mallocSize, "got address", values[0].toString(16));
+                mallocs.push({ addr: values[0], size: mallocSize });
                 mallocLocation = undefined;
             }
         },
-        
-        load(location, op, memarg, value) {
-            console.log(location, op, "value =", value, "from =", memarg);
-        },
+
+        // load(location, op, memarg, value) {
+        //     console.log(location, op, "value =", value, "from =", memarg);
+        // },
 
         store(location, op, memarg, value) {
-            console.log(location, op, "value =", value, "to =", memarg);
-        },
+            let { addr, offset } = memarg;
+            effectiveAddr = addr + offset;
+            let mallocRegion = null;
+            for (malloc of mallocs) {
+                if (effectiveAddr >= malloc.addr && effectiveAddr <= malloc.addr + malloc.size) {
+                    mallocRegion = malloc;
+                }
+                break;
+            }
+            if (mallocRegion !== null) {
+                console.log(op, "of value", "0x" + value.toString(16), "happened at", "0x" + effectiveAddr.toString(16), "in malloced region at addr", "0x" + mallocRegion.addr.toString(16), "of size", mallocRegion.size);
+                updateMallocHistory(op, effectiveAddr, true);
+            } else {
+                console.log(op, "of value", "0x" + value.toString(16), "happened at", "0x" + effectiveAddr.toString(16));
+                updateMallocHistory(op, effectiveAddr, false);
+            }
 
-        memory_size(location, currentSizePages) {
-            console.log(location, "memory_size, size (in pages) =", currentSizePages);
-        },
-
-        memory_grow(location, byPages, previousSizePages) {
-            console.log(location, "memory_grow, delta (in pages) =", byPages, "previous size (in pages) =", previousSizePages);
         },
 
     };
